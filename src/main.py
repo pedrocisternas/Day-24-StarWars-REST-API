d3a09220592bbf8ff4f358c7044cfaeeca7855c0
pedrocisternas <pedrocisternas86@gmail.com>
2021-07-15 01:03:19 +0000

    first

--- src/main.py
+++ src/main.py
@@ -8,7 +8,7 @@ from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import db, User
+from models import db, User, Favorite
 #from models import Person
 
 app = Flask(__name__)
@@ -30,14 +30,30 @@ def handle_invalid_usage(error):
 def sitemap():
     return generate_sitemap(app)
 
-@app.route('/user', methods=['GET'])
+@app.route('/users', methods=['GET'])
 def handle_hello():
-
+    all_users = User.query.all()
+    all_users = list(map(lambda user: user.serialize(),all_users))
     response_body = {
-        "msg": "Hello, this is your GET /user response "
+        "user": all_users
     }
 
     return jsonify(response_body), 200
+
+@app.route('/<user>/favorites', methods=['GET'])
+def get_favorites(user):
+    single_user = User.query.filter_by(username=user).first()
+    if single_user is None:
+        raise APIException("User not found", status_code=404)
+    favorites = Favorite.query.filter_by(username=user)
+    all_favorites = list(map(lambda favorite: favorite.serialize(), favorites))
+    response_body = {
+        "favorites": all_favorites
+    }
+
+    return jsonify(response_body), 200
+
+
 
 # this only runs if `$ python src/main.py` is executed
 if __name__ == '__main__':
--- src/models.py
+++ src/models.py
@@ -6,14 +6,37 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    favorites = db.relationship('Favorite', backref='user')
 
     def __repr__(self):
-        return '<User %r>' % self.username
+        return '<User %r,%r,%r,%r>' % (self.id, self.username, self.email, self.password)
 
     def serialize(self):
         return {
             "id": self.id,
             "email": self.email,
+            "username": self.username,
+            "favorites": [favorite.serialize() for favorite in self.favorites ]
+            # do not serialize the password, its a security breach
+        }
+
+class Favorite(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), unique=False, nullable=False)
+    entity_type = db.Column(db.String(120), unique=False, nullable=False)
+    entity_id = db.Column(db.Integer, unique=False, nullable=False)
+    username = db.Column(db.String(120), db.ForeignKey('user.username'))
+
+    def __repr__(self):
+        return '<Favorite %r,%r,%r,%r,%r>' % (self.id, self.name, self.entity_type, self.entity_id, self.username)
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "entity_type": self.entity_type,
+            "entity_id": self.entity_id,
+            "username": self.username,
             # do not serialize the password, its a security breach
         }
